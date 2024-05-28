a39b352308763b6732c130cfe71026f47c9061d2
Miazzo Luigi <luigi.miazzo.93@gmail.com>
2024-05-28 21:12:29 +0200

    feat: hide httpx logs

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
