626fa29b6e959423db9084da89529ca868a38543
oskarnrk <oskarnrk@users.noreply.github.com>
2025-06-18 23:44:40 +0200

    fix: settings usage

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import holidays
 import logging
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import date
+from datetime import date, timedelta
 from dotenv import load_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
@@ -29,32 +29,35 @@ class Settings(BaseSettings):
     CUSTOM_SKIP_DATES: str = Field(default=None)
 
 
-async def remind_pranzo_tomorrow(bot: Bot, chat_id: str) -> None:
+async def remind_pranzo_tomorrow(bot: Bot, settings: Settings) -> None:
     """Handler to send to chat message for pranzo tomorrow."""
 
     # Check if tomorrow is a holiday in Italy (in the specified province if set)
     # Province codes can be found at https://holidays.readthedocs.io/en/latest/
     if settings.IT_PROVINCE:
-        skip_dates = holidays.IT(prov=settings.IT_PROVINCE)
+        it_holidays = holidays.IT(prov=settings.IT_PROVINCE)
     else:
-        skip_dates = holidays.IT()
+        it_holidays = holidays.IT()
 
-    # Add custom skip dates if provided
-    if settings.CUSTOM_SKIP_DATES: # dates in format "%m-%d"
+    # Add custom skip dates if provided (in format "%d-%m")
+    if settings.CUSTOM_SKIP_DATES:
         custom_skip_dates = settings.CUSTOM_SKIP_DATES.split(",")
         for date_str in custom_skip_dates:
-            day, month = map(int, date_str.split("-"))
-            custom_date = date(year=date.today().year, month=month, day=day)
-            skip_dates.append(custom_date.strftime("%d-%m-%Y"))
+            try:
+                day, month = map(int, date_str.split("-"))
+                custom_date = date(year=date.today().year, month=month, day=day)
+                it_holidays.append(custom_date)
+            except ValueError:
+                logger.warning(f"Invalid date format in CUSTOM_SKIP_DATES: '{date_str}'. Expected format is 'dd-mm'.")
 
-    tomorrow = (await bot.get_me()).date + timedelta(days=1)
-    if tomorrow in skip_dates:
+    tomorrow = date.today() + timedelta(days=1)
+    if tomorrow in it_holidays:
         logger.info("Tomorrow is in the skip days list, not sending poll.")
         return
 
     try:
         await bot.send_poll(
-            chat_id=chat_id,
+            chat_id=settings.CHAT_ID,
             question="Dove mangerai domani? 🍝",
             options=["Ufficio", "Mensa", "Home office", "Ferie/permesso"],
             disable_notification=True,
@@ -75,7 +78,7 @@ async def main():
     async with Bot(settings.TELEGRAM_TOKEN) as bot:
         scheduler.add_job(
             remind_pranzo_tomorrow,
-            kwargs={"bot": bot, "chat_id": settings.CHAT_ID},
+            kwargs={"bot": bot, "settings": settings},
             trigger="cron",
             hour=settings.HOURS,
             minute=settings.MINUTES,
